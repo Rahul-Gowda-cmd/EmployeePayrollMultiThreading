7a11483b43cf6cf97f915ac4df3369e312774bb4
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-09-09 22:35:05 +0530

    feat: Implementation of Multithreadind Concept

---
+++ EmployeePayrollUsingThreads/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollUsingThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Payroll using Threads");
+            string[] words = CreateWordArray(@"http://www.gutenberg.org/files/54700/54700-0.txt");
+
+            #region ParallelTasks
+            Parallel.Invoke(() =>
+            {
+                Console.WriteLine("Begin first task...");
+                GetLongestWord(words);
+            },// close 1st action
+            () =>
+            {
+                Console.WriteLine("Begin second task...");
+                GetMostCommonWords(words);
+            },
+            () =>
+            {
+                Console.WriteLine("Begin third task...");
+                GetCountForWord(words, "sleep");
+            }// close third action
+            );
+            #endregion
+            Console.ReadKey();
+        }
+
+        static string[] CreateWordArray(string uri)
+        {
+            Console.WriteLine($"Retrieving from {uri}");
+            //Download a web page the easy way
+            string blog = new WebClient().DownloadString(uri);
+            return blog.Split(
+                new char[] { ' ', '\u000A', '.', ',', '-', '_', '/' },
+                StringSplitOptions.RemoveEmptyEntries);
+        }
+        private static void GetMostCommonWords(string[] words)
+        {
+            var frequencyOrder = from word in words
+                                 where word.Length > 6
+                                 group word by word into q
+                                 orderby q.Count() descending
+                                 select q.Key;
+            var commonWord = frequencyOrder.Take(10);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Task2 --- the Most comman words are: ");
+            foreach(var v in commonWord)
+            {
+                sb.AppendLine("  " + v);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+        private static string GetLongestWord(string[] words)
+        {
+            string longestWord = (from w in words
+                                  orderby w.Length descending
+                                  select w).First();
+
+            Console.WriteLine($"Task 1 -- The longest word is {longestWord}.");
+            return longestWord;
+        }
+        private static void GetCountForWord(string[] words, string term)
+        {
+            var findWord = from word in words
+                           where word.ToUpper().Contains(term.ToUpper())
+                           select word;
+
+            Console.WriteLine($@"Task 3 -- The word ""{term}"" occurs {findWord.Count()} times.");
+        }
+
+
+    }
+}
